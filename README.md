0eb54821a1db1cf7ef05b7234f0cc9fea610505e
Rodrigo Avellar <roavellarm@gmail.com>
2020-10-06 19:50:27 -0300

    Add project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Reconhecimento de voz
+
+Projeto de exemplo de reconhecimento de voz para transcrição (voz --> texto)
+
---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './App.css'
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from 'react-speech-recognition'
+
+export default function Dictaphone() {
+  const { transcript, resetTranscript } = useSpeechRecognition()
+
+  const listen = () => {
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: 'pt-BR',
+    })
+  }
+
+  if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+    return alert('Este browser não tem suporte para reconhecimento de voz')
+  }
+
+  return (
+    <div className='Container'>
+      <h1 className='Header'>Reconhecimento de voz</h1>
+      <div className='ButtonsContainer'>
+        <button className='Button' onClick={() => listen()}>
+          Começar
+        </button>
+        <button className='Button' onClick={SpeechRecognition.stopListening}>
+          Parar
+        </button>
+        <button className='Button' onClick={resetTranscript}>
+          Limpar
+        </button>
+      </div>
+      <div className='TextContainer'>
+        <h3 className='Title'>Transcrição</h3>
+        <p className='Text'>{transcript}</p>
+      </div>
+    </div>
+  )
+}
